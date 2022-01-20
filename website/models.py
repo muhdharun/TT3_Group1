b87aaeeb8c7d7479eb2b347a1c06013ed9404aa9
Tan Yanta <e0308980@u.nus.edu>
2022-01-20 11:33:20 +0800

    Added LikedPost to models.py, edited user and post to reflect changes.

--- website/models.py
+++ website/models.py
@@ -1,10 +1,12 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchmey import ForeignKey
 
 user_post_table = db.Table('user_project_table',
                               db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
                               db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
 )
+
 
 #Added in password
 class User(db.Model, UserMixin):
@@ -17,11 +19,18 @@ class User(db.Model, UserMixin):
     city = db.Column(db.String(50), nullable=False)
     country = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    posts = db.relationship('Post', secondary=user_post_table, backref=db.backref('user'))
+    liked_posts = db.relationship('LikedPost')
+    posts = db.relationship('Post')
 
 
-class Post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_title = db.Column(db.String(150), nullable=False)
     post_title = db.Column(db.String(100000))
     post_image = db.Column(db.String()) #Not quite sure how database adds in an image
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    liked_posts = db.relationship('LikedPost')
+    
+class LikedPost(db.Model):
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), primary_key=True, autoincrement=False)
+    post_id = db.Column(db.Integer, ForeignKey('post.id'), primary_key=True, autoincrement=False)
