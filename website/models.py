8e673b3d75e731aad75a00f3ea134b93db67f349
Tan Yanta <e0308980@u.nus.edu>
2022-01-20 11:14:52 +0800

    Added Post to model.py, linked to User using backref.

--- website/models.py
+++ website/models.py
@@ -1,6 +1,11 @@
 from . import db
 from flask_login import UserMixin
 
+user_post_table = db.Table('user_project_table',
+                              db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                              db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+)
+
 #Added in password
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,3 +17,11 @@ class User(db.Model, UserMixin):
     city = db.Column(db.String(50), nullable=False)
     country = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    posts = db.relationship('Post', secondary=user_post_table, backref=db.backref('user'))
+
+
+class Post(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.String(150), nullable=False)
+    post_title = db.Column(db.String(100000))
+    post_image = db.Column(db.String()) #Not quite sure how database adds in an image
