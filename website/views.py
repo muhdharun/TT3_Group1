7483e2fe6953288cb33c3f322f78de8b26bbd19d
Tan Yanta <e0308980@u.nus.edu>
2022-01-20 12:05:53 +0800

    Added deletePost into views.py. Will probably need edits later.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from . import db
+from .models import Post
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return "hello world"
+
+@views.route('/deletePost/<int:id>')
+def deletePost(id):
+    post_to_delete = Post.query.get_or_404(id)
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        flash('Post deleted successfully.', category='success')
+        return render_template("home.html", user=current_user)
+    except:
+        flash('Problem occurred while deleting post, try again or contact support.', category='error')
+        return render_template("home.html", user=current_user)
